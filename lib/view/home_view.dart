b8df5aec05ba0787306d112d38a67bd8278706f7
Soner Çiçek <sonercicek013@gmail.com>
2023-11-01 18:09:05 +0300

    add:  name change png

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -17,21 +17,21 @@ class _HomeViewState extends State<HomeView> {
   Widget getWeatherIcon(int code) {
     switch (code) {
       case >= 200 && < 300:
-        return Image.asset('assets/1.png');
+        return Image.asset('assets/tunder.png');
       case >= 300 && < 400:
-        return Image.asset('assets/2.png');
+        return Image.asset('assets/rain.png');
       case >= 500 && < 600:
-        return Image.asset('assets/3.png');
+        return Image.asset('assets/downpour.png');
       case >= 600 && < 700:
-        return Image.asset('assets/4.png');
+        return Image.asset('assets/snow.png');
       case >= 700 && < 800:
-        return Image.asset('assets/5.png');
+        return Image.asset('assets/fog.png');
       case == 800:
-        return Image.asset('assets/6.png');
+        return Image.asset('assets/sunny.png');
       case > 800 && <= 804:
-        return Image.asset('assets/7.png');
+        return Image.asset('assets/partly.png');
       default:
-        return Image.asset('assets/7.png');
+        return Image.asset('assets/partly.png');
     }
   }
 
